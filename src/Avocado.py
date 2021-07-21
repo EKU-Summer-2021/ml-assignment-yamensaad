1a7caccdf630be8dbe6d1734cda2ef6f143e0080
yamensaad <yamen.saa199@gmail.com>
2021-07-21 10:54:59 +0200

    test

---
+++ src/Avocado.py
@@ -0,0 +1,14 @@
+import pandas
+from sklearn import tree
+import pydotplus
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+import matplotlib.image as pltimg
+
+
+df = pandas.read_csv("avocado.csv")
+print(df)
+
+d = {'': 0  , '' :  ,  '' :}
+
+
