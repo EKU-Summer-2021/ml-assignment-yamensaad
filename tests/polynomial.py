76898e819da7c08dd3358d58619450bf8aa6b76d
yamensaad <yamen.saa199@gmail.com>
2021-07-21 10:54:49 +0200

    test

--- tests/polynomial.py
+++ tests/polynomial.py
@@ -3,17 +3,17 @@ import numpy as np
 from src import Polynomial
 
 
-class PolynomialTets(unittest.TestCase):
+class PolynomialTets( unittest.TestCase ):
 
     def test_square_of_three_should_be_nince(self):
         # given
-        p = Polynomial(np.array([1,0,0]))
+        p = Polynomial( np.array( [1, 0, 0] ) )
         X_VALUE = 3
         EXPECTED = 9
         # when
-        ACTUAL = p.evaluate(X_VALUE)
+        ACTUAL = p.evaluate( X_VALUE )
         # then
-        self.assertEqual(EXPECTED, ACTUAL)
+        self.assertEqual( EXPECTED, ACTUAL )
 
     # def test_fails(self):
     #     self.assertEqual(True, False)
